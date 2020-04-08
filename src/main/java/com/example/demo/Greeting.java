3849ce033bd2ae53cbd085848ebf6c625c5a2b55
Karan Nehra <karanehra@gmail.com>
2020-04-08 23:51:02 +0530

    Started on new controller. Edited gitignore

--- src/main/java/com/example/demo/Greeting.java
+++ src/main/java/com/example/demo/Greeting.java
@@ -4,4 +4,17 @@ public class Greeting {
   private final long ID;
   private final String content;
 
+  public Greeting(long ID, String content) {
+    this.ID = ID;
+    this.content = content;
+  }
+
+  public long getID() {
+    return this.ID;
+  }
+
+  public String getContent() {
+    return this.content;
+  }
+
 }
