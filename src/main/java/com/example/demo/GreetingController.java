de5004bb650c0b9b82ec8aee28d0a1c74291db69
Karan Nehra <karanehra@gmail.com>
2020-04-09 23:33:08 +0530

    Connected a model class to controller

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,8 +1,26 @@
 package com.example.demo;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
 public class GreetingController {
+  private static final String template = "Hello %s !";
+  private final AtomicLong counter = new AtomicLong();
 
+  public static void main(String[] args) {
+    SpringApplication.run(GreetingController.class, args);
+  }
+
+  @GetMapping("/greet")
+  public Greeting greet(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return new Greeting(counter.incrementAndGet(), String.format(template, name));
+
+  }
 }
