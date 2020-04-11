0bac05f3c3761effafe043eeef917ec4d4def934
Karan Nehra <karanehra@gmail.com>
2020-04-11 23:52:20 +0530

    Added first post route and resp builder

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -9,8 +9,13 @@ import com.example.demo.repository.IUserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -40,5 +45,11 @@ public class GreetingController {
   @GetMapping("/testget")
   public List<UserModel> getAllUsers() {
     return repository.findAll();
+  }
+
+  @PostMapping("/post")
+  public ResponseEntity<Object> postUser(@RequestBody UserModel user) {
+    repository.save(user);
+    return ResponseEntity.status(HttpStatus.OK).build();
   }
 }
