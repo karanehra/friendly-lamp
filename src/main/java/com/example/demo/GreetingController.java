f971bee801248d00efdaa44e5f9e3f1625cb8350
Karan Nehra <karanehra@gmail.com>
2020-04-18 22:42:37 +0530

    Seperating out controllers

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -7,18 +7,14 @@ import com.example.demo.model.UserModel;
 import com.example.demo.repository.IUserRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
 public class GreetingController {
   private static final String template = "Hello %s !";
@@ -26,10 +22,6 @@ public class GreetingController {
 
   @Autowired
   private IUserRepo repository;
-
-  public static void main(String[] args) {
-    SpringApplication.run(GreetingController.class, args);
-  }
 
   @GetMapping("/greet")
   public Greeting greet(@RequestParam(value = "name", defaultValue = "World") String name) {
--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,5 +1,17 @@
 package com.example.demo.controllers;
 
-public class UserController {
+import com.example.demo.Greeting;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+  @GetMapping("/greet")
+  public Greeting greet(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return new Greeting(0, String.format("Hello %s", name));
+  }
 }
