a95acdb299b5e1748b146980711c229e90e83046
Karan Nehra <karanehra@gmail.com>
2020-06-21 22:31:15 +0530

    Working on parse pipeline

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -2,6 +2,10 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -10,15 +14,34 @@ import org.bson.Document;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WebServer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello");
 
         MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
         MongoDatabase database = mongoClient.getDatabase("testdb");
-        MongoCollection<Document> collection = database.getCollection("articles");
+        MongoCollection<Document> collection = database.getCollection("articlesnew");
         System.out.println(collection.countDocuments());
+
+        URL feedUrl = new URL("https://timesofindia.indiatimes.com/rssfeedstopstories.cms");
+
+        SyndFeedInput feedInput = new SyndFeedInput();
+        SyndFeed feed = feedInput.build(new XmlReader(feedUrl));
+
+        List<SyndEntry> articles = feed.getEntries();
+
+        List<Document> articleDocuments = new ArrayList<>();
+
+        for (SyndEntry article : articles) {
+            Document doc = new Document("title",article.getTitle()).append("description", article.getDescription().getValue());
+            articleDocuments.add(doc);
+        }
+        collection.insertMany(articleDocuments);
+
     }
 
     void setupHttpServer() throws IOException {
---
+++ src/main/java/models/User.java
@@ -0,0 +1,11 @@
+package models;
+
+public class User {
+    private final String firstName;
+    private final String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
